4919aa0705e685cce298a8b8478e9dc6009edeb9
Volodymyr Marchenko <Katalizator1992@gmail.com>
2022-12-07 04:12:30 +0200

    finish-commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const mainGallery = galleryItems
+  .map(
+    (item) => `<div class ="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" 
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"/>
+        </a>
+        </div>`
+  )
+  .join("");
+gallery.innerHTML = mainGallery;
+gallery.addEventListener("click", onclick);
+
+function onclick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src= ${e.target.dataset.source} source= ${e.target.src}>
+`);
+
+  instance.show();
+  document.addEventListener("keydown", escape);
+  function escape(e) {
+    if (e.code === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", escape);
+    }
+  }
+}
+
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const mainGallery = galleryItems
+  .map(
+    (item) => `<li>
+    <a href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>
+</li>`
+  )
+  .join("");
+gallery.innerHTML = mainGallery;
+
+gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionsData: "alt",
+  captionDelay: 250,
+  captionSelector: "img",
+  captionPosition: "bottom",
+});
